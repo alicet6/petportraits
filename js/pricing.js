032f604a351d9f6b32e5d70aa5052c5eb0384c43
Alice Thompson <alicethompsonphotography@gmail.com>
2015-11-14 23:22:07 -0500

    added pricing selection box to contact form

--- js/pricing.js
+++ js/pricing.js
@@ -1,12 +1,12 @@
 var packages = [{
   packageid: 0,
   name: "Package 1 - $350",
-  description: ["One free 16 x 20 photo of your choice", "a CD of up to 35 low-resolution watermarked photos, sized appropriately for social media", "Also 25% off any additional A la cart products"],
+  description: ["One free 16 x 20 photo of your choice, a CD of up to 35 low-resolution watermarked photos (sized appropriately for social media), and 25% off any additional A la cart products"],
   price: 350
 }, {
   packageid: 1,
   name: "Package 2 - $250",
-  description: "One free 11 x 14 photo of your choice, plus a CD of up to 25 low-resolution watermarked photos, sized appropriately for social media. Also 20% off any additional A la cart products.",
+  description: "One free 11 x 14 photo of your choice, a CD of up to 25 low-resolution watermarked photos (sized appropriately for social media), and 20% off any additional A la cart products.",
   price: 250
 }, {
   packageid: 2,
